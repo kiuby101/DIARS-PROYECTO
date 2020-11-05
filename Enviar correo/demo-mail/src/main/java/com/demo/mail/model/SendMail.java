99879e48d7aeaf940e985d9b7543aef44163be3f
Axel Alejandro Holguin Andrade <N00092260@upn.pe>
2020-11-04 22:40:33 -0500

    My team
    Avance de Enviar correo

---
+++ Enviar correo/demo-mail/src/main/java/com/demo/mail/model/SendMail.java
@@ -0,0 +1,37 @@
+package com.demo.mail.model;
+
+public class SendMail {
+
+	private String subject;
+	private String to;
+	private String from;
+	private String text;
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+	
+}
