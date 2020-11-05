1e008acbc9fda71ea0accd1e5d70d480d3d18c16
nandoxx321 <mini_motta2012@hotmail.com>
2020-11-04 22:26:38 -0500

    dsa
    asd

--- spring-boot-ecommerce/src/main/java/com/nagro/springboot/app/model/service/IAdministradorService.java
+++
@@ -1,8 +0,0 @@
-package com.nagro.springboot.app.model.service;
-
-import com.nagro.springboot.app.model.entity.Administrador;
-
-public interface IAdministradorService {
-    public boolean findByMailAndPassword(String email,String password);
-    public void Save(Administrador administrador);
-}
