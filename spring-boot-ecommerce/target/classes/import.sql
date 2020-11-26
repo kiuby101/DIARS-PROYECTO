419b1b2b052cfffcb19c398ad6a44ff7280618a3
nandoxx321 <mini_motta2012@hotmail.com>
2020-11-26 00:17:16 -0500

    proycto
    proyeto

---
+++ spring-boot-ecommerce/target/classes/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES(1,'Andres','Guzman','profesor@gmail.com','2017-08-08');
+INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES(2,'Andres','Saldana','andresr@gmail.com','2017-08-08');
+
+INSERT INTO detalle_productos(id,detalle) VALUES(1,"erkoqwkro - qpqwekrpokwe-qwer14213123-3431423qewrqw-pqwoerkpowke-pqkweork");
+INSERT INTO detalle_productos(id,detalle) VALUES(2,"erkoqwkro - qpqwekrpokwe-rpwqkeropwepkor-qpwekropqwekr-pqwoerkpowke-pqkweork");
+
+
+INSERT INTO tipo_productos(id,nombre) VALUES(1,"teclado");
+INSERT INTO tipo_productos(id,nombre) VALUES(2,"monitores");
+INSERT INTO tipo_productos(id,nombre) VALUES(3,"laptops");
+INSERT INTO tipo_productos(id,nombre) VALUES(4,"camaras wifi");
+INSERT INTO tipo_productos(id,nombre) VALUES(5,"camaras");
+INSERT INTO tipo_productos(id,nombre) VALUES(6,"mouse");
+INSERT INTO tipo_productos(id,nombre) VALUES(7,"mouse gamer");
+
+INSERT INTO administradores(id,mail,password) VALUES(1,"root@root.com","root"); 
